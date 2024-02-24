26fe7ec928515308dafc337103dd572026cc522f
AGLIFAH ALFARABI <aglifahalfb4@gmail.comgit config --global user.name AglifahAlfarabiBasri>
2024-02-24 19:50:08 +0700

    Codelab 1

--- Codelab.java
+++ Codelab.java
@@ -4,7 +4,7 @@ import java.time.LocalDate;
 import java.time.Period;
 import java.util.Scanner;
 
-public class App 
+public class Codelab
 {
     public static void main( String[] args ) {
         Scanner scanner = new Scanner(System.in);
@@ -12,7 +12,7 @@ public class App
         System.out.print("Nama : ");
         String nama = scanner.nextLine();
 
-        System.out.println("Jenis kelamin (L/P)");
+        System.out.println("Jenis kelamin ? JUJUR (L/P)");
         char JenisKelamin = scanner.nextLine().charAt(0);
         String JenisKelaminString = (JenisKelamin == 'L' || JenisKelamin == 'l') ? "Laki-laki" : "Perempuan";
 
@@ -25,7 +25,7 @@ public class App
         int bulan = Period.between(TanggalLahir, TanggalSekarang).getMonths();
 
         System.out.println("\ndata diri:");
-        System.out.println("Nama : " + nama);
+        System.out.println("Namamu : " + nama);
         System.out.println("Jenis kelamin :" + JenisKelaminString);
         System.out.println("Umur : " + umur + " tahun " + bulan + " Bulan ");
         
